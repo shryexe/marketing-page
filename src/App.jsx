00aa4f567a4760a8404b9eaa2d4ed21c39cc4c5c
shreya <shreya.mishra.cs24@ggits.net>
2025-12-14 00:34:31 +0530

    Add main App component to structure the marketing page layout

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Hero from './sections/Hero'
+import Features from './sections/Features'
+import About from './sections/About'
+import Testimonials from './sections/Testimonials'
+import Pricing from './sections/Pricing'
+import Footer from './sections/Footer'
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Hero />
+      <Features />
+      <About />
+      <Testimonials />
+      <Pricing />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
+
+
+
